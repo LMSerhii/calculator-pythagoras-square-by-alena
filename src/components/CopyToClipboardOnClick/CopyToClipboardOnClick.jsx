45ba48d745eac81b630b178825876d5bb7825344
LMSerhii <117682588+LMSerhii@users.noreply.github.com>
2024-03-27 11:50:54 +0200

    copy clipboard style

--- src/components/CopyToClipboardOnClick/CopyToClipboardOnClick.jsx
+++ src/components/CopyToClipboardOnClick/CopyToClipboardOnClick.jsx
@@ -1,18 +1,32 @@
 import { useState } from 'react';
 import s from './CopyToClipboardOnClick.module.css';
+import { useMediaQuery } from 'usehooks-ts';
 
 const CopyToClipboardOnClick = ({ text }) => {
   const [copied, setCopied] = useState(false);
 
-  const handleCopy = () => {
+  const to_mq768 = useMediaQuery('(max-width: 767.98px)');
+  const mq768 = useMediaQuery('(min-width: 768px)');
+  const mq1440 = useMediaQuery('(min-width: 1440px)');
+
+  const handleCopy = e => {
+    e.preventDefault();
     navigator.clipboard.writeText(text);
     setCopied(true);
     setTimeout(() => setCopied(false), 2000);
   };
 
+  const text_to_768 = `${text.slice(0, 25)}...`;
+  const text_768 = `${text.slice(0, 50)}...`;
+  const text_1440 = `${text.slice(0, 58)}...`;
+
   return (
-    <div className={s.wrapper} onClick={handleCopy}>
-      <p className={s.text}>{text}</p>
+    <div className={s.wrapper}>
+      <button className={s.text} onClick={handleCopy}>
+        {to_mq768 && text_to_768}
+        {mq768 && text_768}
+        {mq1440 && text_1440}
+      </button>
       {copied ? 'Copied!' : 'Click to Copy'}
     </div>
   );
